c62cf057e49577541d0a91e54976d861f2e09873
mara-olson <mara.c.olson@gmail.com>
2022-03-26 13:08:02 -0700

    Completed functions for creating blank letter lines, letter buttons for the alphabet, and disabling a hard-coded letter

--- static/js/sharkwords.js
+++ static/js/sharkwords.js
@@ -1,18 +1,18 @@
-const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
 const WORDS = [
-  'strawberry',
-  'orange',
-  'apple',
-  'banana',
-  'pineapple',
-  'kiwi',
-  'peach',
-  'pecan',
-  'eggplant',
-  'durian',
-  'peanut',
-  'chocolate',
+  "strawberry",
+  "orange",
+  "apple",
+  "banana",
+  "pineapple",
+  "kiwi",
+  "peach",
+  "pecan",
+  "eggplant",
+  "durian",
+  "peanut",
+  "chocolate",
 ];
 
 const numWrong = 0;
@@ -20,13 +20,25 @@ const numWrong = 0;
 // Loop over the chars in `word` and create divs.
 // The divs should be appended to the section with id="word-container".
 const createDivsForChars = (word) => {
-  // Replace this with your code
+  for (const char of word) {
+    const container = document.querySelector("#word-container");
+
+    container.insertAdjacentHTML(
+      "beforeend",
+      `<div class='letter-box' ${char}></div>`
+    );
+  }
 };
 
 // Loop over each letter in the alphabet and generate a button for each letter
 // The buttons should be appended to the section with id="letter-buttons"
 const generateLetterButtons = () => {
   // Replace this with your code
+  for (const letter of ALPHABET) {
+    const button = document.querySelector("#letter-buttons");
+
+    button.insertAdjacentHTML("beforeend", `<button>${letter}</button>`);
+  }
 };
 
 // Set the `disabled` property of `buttonEl` to `true.
@@ -35,6 +47,7 @@ const generateLetterButtons = () => {
 //
 const disableLetterButton = (buttonEl) => {
   // Replace this with your code
+  buttonEl.setAttribute("disabled", "true");
 };
 
 // This is a helper function we will use in the future
@@ -53,13 +66,17 @@ const isLetterInWord = (letter) => {
   // You can change this to choose a random word from WORDS once you
   // finish this lab but we hard code it so we know what the word is
   // and can tell if things look correct for this word
-  const word = 'hello';
+  const word = "hello";
 
   // call the function that makes an empty line for each letter in the word
-  // Replace this line with the function call
+  createDivsForChars(word);
 
   // call the function that makes a button for each letter in the alphabet
-  // Replace this line with the function call
+  generateLetterButtons();
+
+  buttonEl = document.querySelector("#letter-buttons > button:nth-child(8)");
+
+  disableLetterButton(buttonEl);
 
   // in the next lab, you will be adding functionality to handle when
   // someone clicks on a letter
